7d04f4b0d8b72f26844c32069c8a26514572c7dd
SantiRomano03 <romanosantiagonicolas@gmail.com>
2025-07-31 19:52:27 -0300

    Fix PK

--- models/tourist_site.py
+++ models/tourist_site.py
@@ -3,7 +3,7 @@ from models.db import db
 class TouristSite (db.Model):
     __tablename__ = "tourist_site"
 
-    id_tourist_site = db.Column (db.Integer, primary_key = True)
+    id_tourist_site = db.Column (db.Integer,unique = True, primary_key = True)
     name = db.Column (db.String(50),unique = True, nullable = False)
     description = db.Column (db.String(250), nullable = False)
     address = db.Column(db.String(50), unique = True, nullable = False)
