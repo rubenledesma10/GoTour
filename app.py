2787703628a147b511b2a3e11129d711869c3da8
rubenledesma10 <rubenledesma89@gmail.com>
2025-09-21 16:42:25 -0300

    add seed.py and class config

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
 from flask import Flask, render_template
 from flask_migrate import Migrate
-from config.config import DATABASE_CONNECTION_URI
 from models.db import db
 from routes.user_route import user_bp
 from routes.admin_route import admnin_bp
 from routes.tourist_route import tourist_bp
 from routes.receptionist_route import recepcionist_bp
+from config.config import Config
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
 from config.email_config import init_mail
 
 app=Flask(__name__)
-app.config['JWT_SECRET_KEY']='tu_clave_secreta' #definimos clave secreta para firmar los tokens
-app.config["JWT_ACCESS_TOKEN_EXPIRES"]=timedelta(hours=1)
+app.config.from_object(Config)
 jwt=JWTManager(app) #inicializamos jwt en la aplicacion 
 init_mail(app) #inicializamos correo
+db.init_app(app) #inicializamos bd
+migrate = Migrate(app, db)
+
+#registramos bluesprints
 app.register_blueprint(user_bp)
 app.register_blueprint(admnin_bp)
 app.register_blueprint(tourist_bp)
 app.register_blueprint(recepcionist_bp)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-migrate = Migrate(app, db)
-
 
 
 with app.app_context():
