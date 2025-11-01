3af828e4b1f489c0ff8a38a143cf7e38b6833626
rubenledesma10 <rubenledesma89@gmail.com>
2025-11-01 19:43:59 -0300

    fix test mock

--- test/tests_mock.py
+++ test/tests_mock.py
@@ -2,14 +2,14 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import importlib
+import importlib #importamos el modulo desp de mockear
 import pytest
 from flask import Flask
-from unittest.mock import patch
-from functools import wraps
+from unittest.mock import patch #para reemplazar el decorador real
+from functools import wraps #para que el wrapper mantenga el nombre de la función original.
 
 
-class DummyUser:
+class DummyUser: #usuario trucho que vamos a inyectar en la vista como si fuese un current user
     def __init__(self):
         self.id_user = "123"
         self.username = "admin_fake"
@@ -24,26 +24,25 @@ def app():
 
 
 def test_admin_welcome_ok(app):
-    # 1) mockeamos el decorador ORIGINAL antes de importar el blueprint
-    def fake_role_required(required_role):
+    def fake_role_required(required_role): #decorador falso para reempalzar el real
         def decorator(fn):
-            @wraps(fn)     # 👉 mantiene el nombre: no choca endpoints
+            @wraps(fn)     # mantenemos el nombre: no choca endpoints
             def wrapper(*args, **kwargs):
                 dummy = DummyUser()
-                # tus vistas reciben current_user como primer arg
+                # las vistas reciben current_user como primer arg
                 return fn(dummy, *args, **kwargs)
             return wrapper
         return decorator
 
     with patch("utils.decorators.role_required", new=fake_role_required):
-        # 2) ahora sí importamos el módulo que usa ese decorador
+        #ahora importamos el módulo que usa ese decorador
         admin_route = importlib.import_module("routes.admin_route")
 
-        # 3) registramos el blueprint
+        # registramos el blueprint
         app.register_blueprint(admin_route.admin_bp)
         client = app.test_client()
 
-        # 4) probamos
+        # probamos
         resp = client.get("/api/admin/welcome")
         assert resp.status_code == 200
         data = resp.get_json()
