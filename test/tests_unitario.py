9b4c54f84bf0048c3841f31285f4a35ed21abcb9
rubenledesma10 <rubenledesma89@gmail.com>
2025-11-01 12:50:32 -0300

    add 1 test unit

--- test/tests_unitario.py
+++ test/tests_unitario.py
@@ -15,17 +15,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:" #bd temporal en mem
 @pytest.fixture
 def setup_db():
     with app.app_context():
-        db.create_all()
-        yield
-        db.drop_all()
+        db.create_all() #crea las tablas
+        yield #aca va el test que usa la bd
+        db.drop_all() #borra las tablas al final para que no quede nada
 
 
-def test_user_model_create(setup_db):
-    # Crear instancia de usuario
-    user = User(
+def test_user_model_create(setup_db): #el parametro setup_db hace que se ejecute el fixture antes y despues el test
+    user = User( #creamos la instancia de user
         first_name="Juan",
         last_name="Pérez",
-        email="juan@example.com",
+        email="juanperez@example.com",
         password="password123",
         username="juanp",
         role="tourist",
@@ -41,13 +40,13 @@ def test_user_model_create(setup_db):
 
     with app.app_context():
         db.session.add(user)
-        db.session.commit()
+        db.session.commit() 
 
-        saved = User.query.filter_by(email="juan@example.com").first()
+        saved = User.query.filter_by(email="juan@example.com").first() #buscamos el usuario
 
-        assert saved is not None
-        assert saved.username == "juanp"
-        assert saved.check_password("password123") is True  # ✅ Verifica el hash
+        assert saved is not None #verificamos que se creo el usuario
+        assert saved.username == "juanp" #verificamos que le username se guardo correcamente
+        assert saved.check_password("password123") is True  #verificamos que la contraseña hash funcione correctamente
         assert saved.role == "tourist"
         assert saved.dni == "12345678"
         assert saved.nationality == "Argentina"
